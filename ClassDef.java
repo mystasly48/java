850fa0c3fecb1616ef34c8b832575ebbfe008115
Maisu <mystasly48@gmail.com>
2016-12-12 05:10:30 +0000

    Create ClassDef.java

---
+++ ClassDef.java
@@ -0,0 +1,9 @@
+public class ClassDef {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("I'm");
+    sb.append(" a");
+    sb.append(" Coder!");
+    System.out.println(sb.toString());
+  }
+}
